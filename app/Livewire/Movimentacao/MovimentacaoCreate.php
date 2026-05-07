0e78370b31264b2a1c68e7b3259b1f4fbd3f6930
asafhzinn <hudsonasaf080@gmail.com>
2026-05-07 10:55:07 -0300

    Aula dia 07/05/2026

--- app/Livewire/Movimentacao/MovimentacaoCreate.php
+++ app/Livewire/Movimentacao/MovimentacaoCreate.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Movimentacao;
 
+use App\Models\Movimentacao;
 use App\Models\Produto;
 use Livewire\Component;
 
@@ -22,5 +23,49 @@ class MovimentacaoCreate extends Component
     public function render()
     {
         return view('livewire.movimentacao.movimentacao-create');
+    }
+
+    public function store(){
+        $produto = Produto::find($this->idProdutoSelecionado);
+
+        if($produto->qtd_estoque < $this->quantidade_movimentacao && $this->tipo == 'saida'){
+            $this->addError('quantidade_movimentacao','Quantidade em estoque insuficiente');
+            return;
+        }
+
+        // Atualizar estoque
+        if($this->tipo == "entrada"){
+           // $produto->qtd_estoque =  $produto->qtd_estoque + $this->quantidade_movimentacao;
+           $produto->qtd_estoque += $this->quantidade_movimentacao;
+           //$produto->increment('qtd_estoque',$this->quantidade_movimentacao);
+
+        }else{
+              // $produto->qtd_estoque =  $produto->qtd_estoque + $this->quantidade_movimentacao;
+          // $produto->qtd_estoque += $this->quantidade_movimentacao;
+           $produto->decrement('qtd_estoque',$this->quantidade_movimentacao);
+        }
+
+        //Registrar Movimentacao
+        Movimentacao::create([
+            'quantidade'=> $this->quantidade_movimentacao,
+            'data_movimentacao'=> $this->data_movimentacao,
+            'tipo' => $this->tipo,
+            'produto_id' => $this->idProdutoSelecionado,
+            'user_id' => 1
+        ]);
+
+        $produto->update();
+
+        //verificar estoque baixo
+        $produto->refresh();
+        if($produto->qtd_estoque < $produto->qtd_minima){
+            $this->alertaEstoqueBaixo = "ALERTA: Estoque baixo para {$produto->nome}. Quantidade Atual:{$produto->qtd_estoque}";
+
+        }else{
+            $this->alertaEstoqueBaixo="";
+        }
+        session()->flash('message','Movimentação registrada com sucesso!');
+        $this->reset(['quantidade_movimentacao','tipo']);
+        $this->produtos = Produto::orderby('nome')->get();
     }
 }
